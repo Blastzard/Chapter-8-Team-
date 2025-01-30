e79433bb342abac644ed37e405cbb4b4a70590bd
Seb4354 <142840057+Seb4354@users.noreply.github.com>
2025-01-30 14:43:53 -0600

    fgcd

--- analyze.cpp
+++ analyze.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-void analyze()
+void analyze(vector<string>students, vector<vector<double>> grades, int studentnumber, int gradenumber)
 {
 
 }
--- gradeoutput.cpp
+++ gradeoutput.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void grade_ouput(vector<vector<double>> grades, vector<string>students)
+void grade_ouput(vector<string>students, vector<vector<double>> grades, int studentnumber, int gradenumber)
 {
 
 }
