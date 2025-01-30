ffbb9e4fba27adb9079eb7b601a35a79fe9533d5
Seb4354 <142840057+Seb4354@users.noreply.github.com>
2025-01-30 14:14:48 -0600

    Update gradeinput.cpp

--- gradeinput.cpp
+++ gradeinput.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
-int grade_input()
+double grade_input()
 {
-
+	double grade;
+	cout << "\nenter the grade of the student; ";
+	cin >> grade;
+	while (cin.fail())
+	{
+		cout << "\nenter a valid grade of the student; ";
+		cin >> grade;
+	}
+	return grade;
 }
