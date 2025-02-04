6f76d2a6c323a55a20a7dddcc280bae7f16ced6d
GideonPeine <142840190+GideonPeine@users.noreply.github.com>
2025-02-04 14:10:21 -0600

    Update sort.cpp
    adds sort

--- sort.cpp
+++ sort.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-void sort()
-{
 
+void sort(vector<int>& list) {
+    sort(list.begin(), list.end());
+
+    cout << "Sorted list: ";
+    for (int num : list) {
+        cout << num << " ";
+    }
+    cout << endl;
 }
